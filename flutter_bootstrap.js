d1479af301b2726f76b4ecb24a120da1129338a3
Franco Dall'Ongaro <francodallongaro@gmail.com>
2025-11-17 10:44:18 +0100

    new version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "669345286"
+    serviceWorkerVersion: "1778171205"
   }
 });
